8235bcbb554238fc426b59bb990063b8398e683c
Archmage Global <abhiram.c@icloud.com>
2026-07-09 01:41:16 +0530

    fix: switch docs typography to Graphik Web

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
-import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
 import { Provider } from '@/components/provider';
 import './global.css';
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`} suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body className="flex flex-col min-h-screen font-sans" suppressHydrationWarning>
         <Provider>{children}</Provider>
       </body>
